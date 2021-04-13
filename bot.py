5c641a617cbf62d98a11ac3abbe5733f6b582ac8
Sorcier88 <>
2021-04-13 14:16:37 +0000

    Initial commit

---
+++ bot.py
@@ -0,0 +1,12 @@
+import json
+import os
+import discord
+# quotes = json.load(open("engiQuotes.txt"))
+voiceQuotes = os.listdir("Sounds/")
+token = ""
+client = discord.Client()
+try:
+    # We can't include token.txt in the repository as it is private
+    token = os.getenv('TOKEN')
+except FileNotFoundError as e:
+    print(e)
